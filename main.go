8605bce4b86edb4d30789a90ab26aadfc6d5054b
CodeLumen | Mike <mike@codelumen.nl>
2025-10-28 23:52:07 +0100

    feat: update code to use json-configuration

--- main.go
+++ main.go
@@ -1,48 +1,66 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 	"syscall"
 	"time"
 )
 
+type EbpfConfig struct {
+	Enabled      bool          `json:"enabled"`
+	Command      string        `json:"command"`
+	Args         []string      `json:"args"`
+	PollInterval time.Duration `json:"poll_interval"`
+}
+
 func main() {
-	const filePath = "enable_profiler.txt"
-	var lastValue string
+	const filePath = "config.json"
+	var lastValue bool
 	var processId = -1
+	var pollInterval = 2 * time.Second
 
 	for {
 		data, err := os.ReadFile(filePath)
 		if err != nil {
 			log.Printf("Error reading file: %v", err)
-			time.Sleep(2 * time.Second)
+			time.Sleep(pollInterval)
 			continue
 		}
 
-		value := strings.TrimSpace(string(data))
-		if value != lastValue {
-			if value == "true" {
-				log.Println("Profiler changed to true!")
-				processId = executeProfiler()
+		var config EbpfConfig
+		err = json.Unmarshal(data, &config)
+		if err != nil {
+			log.Printf("Error parsing json: %v", err)
+			time.Sleep(pollInterval)
+			continue
+		}
+
+		if config.Enabled != lastValue {
+			log.Printf("Profiler changed to %v", config.Enabled)
+			if config.Enabled {
+				processId = executeProfiler(config.Command, config.Args...)
 			} else {
-				log.Printf("Profiler changed to %q", value)
+
 				if processId > 0 {
 					stopProfiler(processId)
 					processId = -1
 				}
 			}
-			lastValue = value
+			lastValue = config.Enabled
 		}
 
-		time.Sleep(2 * time.Second)
+		pollInterval = config.PollInterval * time.Second
+		time.Sleep(pollInterval)
 	}
 }
 
-func executeProfiler() int {
-	cmd := exec.Command("ping", "8.8.8.8")
+func executeProfiler(command string, args ...string) int {
+	fmt.Printf("Executing: %v %v\n", command, args)
+	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	err := cmd.Start()
 	if err != nil {
